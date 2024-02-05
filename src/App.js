c105fa5366288719022dcee7dfe82bcd4414379b
HifzonHifzon <hifzon746@gmail.com>
2024-02-05 22:58:37 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+import Sidebar from './components/Sidebar.js'
+import Footer from './components/Footer.js'
+import Content from './components/Content.js'
+import ControlSidebar from './components/ControlSidebar.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div class="wrapper">
+        <Header/>
+        <Sidebar/>
+        <Content/>
+        <Footer/>
+        <ControlSidebar/>
+      </div>
+      </div>
   );
 }
 
